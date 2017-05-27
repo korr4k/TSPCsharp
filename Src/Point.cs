ff54a09d2c3d0eaac700b227a8ea6a75a5118bdc
CDB <korrak93@gmail.com>
2017-05-27 07:58:19 +0200

    Distanze aggiornate

--- Src/Point.cs
+++ Src/Point.cs
@@ -51,19 +51,65 @@ namespace TSPCsharp
 
             if (pointType == "EUC_2D")
             {
-                //Ceiling is used to round at the next int value
-                return (int)(Math.Sqrt(xD * xD + yD * yD) + 0.5);
+                return Convert.ToInt32(Math.Sqrt(xD * xD + yD * yD) + 0.5);
+            }
+            else if (pointType == "MAN_2D")
+            {
+                xD = Math.Abs(xD);
+                yD = Math.Abs(yD);
+                return Convert.ToInt32(xD + yD + 0.5);
+            }
+            else if (pointType == "MAX_2D")
+            {
+                xD = Math.Abs(xD);
+                yD = Math.Abs(yD);
+
+                int fI = Convert.ToInt32(xD + 0.5);
+                int sI = Convert.ToInt32(yD + 0.5);
+                if (fI >= sI)
+                    return fI;
+                else
+                    return sI;
+            }
+            else if (pointType == "GEO")
+            {
+                double PI = 3.141592;
+
+                int deg = Convert.ToInt32(p1.X);
+                double min = p1.X - deg;
+                double latitude1 = PI * (deg + 0.5 * min / 3.0) / 180;
+                deg = Convert.ToInt32(p1.Y);
+                min = p1.Y - deg;
+                double longitude1 = PI * (deg + 0.5 * min / 3.0) / 180;
+
+                deg = Convert.ToInt32(p2.X);
+                min = p2.X - deg;
+                double latitude2 = PI * (deg + 0.5 * min / 3.0) / 180;
+                deg = Convert.ToInt32(p2.Y);
+                min = p2.Y - deg;
+                double longitude2 = PI * (deg + 0.5 * min / 3.0) / 180;
+
+                double RRR = 6378.388;
+                double q1 = Math.Cos(longitude1 - longitude2);
+                double q2 = Math.Cos(latitude1 - latitude2);
+                double q3 = Math.Cos(latitude1 + latitude2);
+
+                return Convert.ToInt32((RRR * Math.Acos(0.5 * ((1.0 + q1) * q2 - (1.0 - q1) * q3))) + 1.0);
             }
             else if (pointType == "ATT")
             {
-                double tmp = Math.Sqrt((xD * xD + yD * yD) / 10.0);
+                double rij = Math.Sqrt((xD * xD + yD * yD) / 10.0);
+                int tij = Convert.ToInt32(rij);
 
-                if ((int)(tmp + 0.5) < tmp)
-                    return (int)tmp + 1;
+                if (tij < rij)
+                    return tij + 1;
                 else
-                    return tmp;
+                    return tij;
             }
-
+            else if (pointType == "CEIL_2D")
+            {
+                return Math.Ceiling(Math.Sqrt(xD * xD + yD * yD) + 0.5);
+            }
             //If each statem is false, the used point type is not yet implemented
             throw new Exception("Bad input format");
         }        
